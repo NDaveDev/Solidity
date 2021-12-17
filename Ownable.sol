0d6db10fc314b3c0f98a2fefe02ab0910dcbedeb
NDaveDev <96290062+NDaveDev@users.noreply.github.com>
2021-12-17 18:11:53 +0800

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -3,7 +3,7 @@
 
 pragma solidity >=0.6.0 <0.8.0;
 
-import "../utils/Context.sol";
+import "https://github.com/NDaveDev/Solidity/blob/main/Context.sol";
 
 /**
  * @dev Contract module which provides a basic access control mechanism, where
