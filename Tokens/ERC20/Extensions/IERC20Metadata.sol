0e02ae42bb899a6a4616ca7d76791fc332284396
NDaveDev <96290062+NDaveDev@users.noreply.github.com>
2021-12-17 18:45:45 +0800

    Update IERC20Metadata.sol

--- Tokens/ERC20/Extensions/IERC20Metadata.sol
+++ Tokens/ERC20/Extensions/IERC20Metadata.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts v4.4.1 (token/ERC20/extensions/ERC20Burnable.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity >=0.6.0 <0.8.0;
 
 import "https://github.com/NDaveDev/Solidity/blob/main/Tokens/ERC20/IERC20.sol";
 
