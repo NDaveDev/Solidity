fb53e9a5155be3297e07da15c7282973e85fae73
NDaveDev <96290062+NDaveDev@users.noreply.github.com>
2021-12-17 18:39:54 +0800

    Update ERC20.sol

--- Tokens/ERC20/ERC20.sol
+++ Tokens/ERC20/ERC20.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts v4.4.1 (token/ERC20/ERC20.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity >=0.6.0 <0.8.0;
 
 import "https://github.com/NDaveDev/Solidity/blob/main/Tokens/ERC20/IERC20.sol";
-import "https://github.com/NDaveDev/Solidity/blob/main/Tokens/ERC20/IERC20Metadata.sol";
+import "https://github.com/NDaveDev/Solidity/blob/main/Tokens/ERC20/Extensions/IERC20Metadata.sol";
 
 /**
  * @dev Implementation of the {IERC20} interface.
